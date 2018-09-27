cef624a1c9bc6bc626eae037cc142e58e94ce8fb
Matthew Pollock <mattpollock4@gmail.com>
2018-09-27 12:50:53 -0400

    Commiting some stuff

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ var cors = require('cors')
 var querystring = require('querystring')
 var cookieParser = require('cookie-parser')
 var admin = require("firebase-admin")
-var se
+var serviceAccount = require("./PrivateKeys/party-haus-217716-555c9903fe9d.json")
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://party-haus.firebaseio.com/"
+})
+var db = admin.database
 
