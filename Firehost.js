c41a01cce9b4e1997357f4ff58b82f47f7cec9fa
Matthew Pollock <mattpollock4@gmail.com>
2018-09-27 23:15:47 -0400

    JavaScript Fix

---
+++ Firehost.js
@@ -0,0 +1,51 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCAAfW74JRokE1cWI_J4N-k8o2BWfdJsLY",
+  authDomain: "party-haus.firebaseapp.com",
+  databaseURL: "https://party-haus.firebaseio.com",
+  projectId: "party-haus",
+  storageBucket: "party-haus.appspot.com",
+  messagingSenderId: "468631990782"
+};
+firebase.initializeApp(config);
+
+var db = firebase.database();
+
+function getQueryVariable(id) 
+{
+  let query = window.location.search.substring(1);
+  let vars = query.split('?');
+  for (let i = 0; i<vars.length;i++) {
+    let pair = vars[i].split('=');
+    if(pair[0] == id){
+      return pair[1];
+    }
+
+  }
+  return(false);
+}
+
+function getPartyName()
+{
+  accessCode = getQueryVariable('accessCode');
+  party = db.ref(accessCode+'/')
+  return party.once('value').then(function(snapshot) {
+    let partyName = snapshot.val().partyName;
+    return partyName;
+  });
+  
+
+}
+
+window.onload = function() {
+  let code = getQueryVariable('accessCode');
+  let name = getPartyName();
+  name.then(function(value) {
+    document.getElementById("party-name").innerHTML = value 
+  })
+  document.getElementById("party-code").innerHTML = code;
+}
+
+
+
+
